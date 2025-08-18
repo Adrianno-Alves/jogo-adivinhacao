b420fbb8421e2e19a08aaa695e25a3b8f3981584
Adrianno-Alves <adriannopereiraalves@gmail.com>
2025-08-18 20:02:49 -0300

    Create jogo.py

---
+++ jogo.py
@@ -0,0 +1,25 @@
+python
+
+import random
+
+def jogo() :
+  print("🎲 Bem-vindo ao jogo da Adivinhação 🎲")
+  print("O computador vai pensar em um número entre 1 e 100...")
+
+numero_secreto = random.randint(1, 100)
+tentativas = 0
+acertou = False
+
+while not acertou:
+  palpite = int(imput("Digite seu palpite: ")
+  tentativas += 1
+
+  if palpite == numero_secreto:
+    print(f"  Parabens! Vece acertou o numero {numero_secreto} em {tentativas} tentativas.")
+    acertou = True
+  elif palpite < numer_secreto:
+    print("O numero é MAIOR. Tente novamente!)
+          else:
+    print("O numero é MENOR. Tente novamente!")
+if __name__ == "__main__":
+  jogo()
